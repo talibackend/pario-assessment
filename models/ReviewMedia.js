c1f71fb83683116a43c12235c576782f49007db2
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2022-08-22 00:01:18 +0100

    Update ReviewMedia.js

--- models/ReviewMedia.js
+++ models/ReviewMedia.js
@@ -1,2 +1,24 @@
 const {Model, DataTypes} = require("sequelize");
 const dbInstance = require("../database/sequelize");
+const Review = require("./Review");
+
+class ReviewMedia extends Model {}
+
+ReviewMedia.init({
+    id : {
+        type : DataTypes.INTEGER,
+        primaryKey : true,
+        autoIncrement : true,
+        allowNull : false
+    },
+    review_id : {
+        type : DataTypes.INTEGER,
+        references : {
+            model : Review,
+            key : "id"
+        }
+    },
+    media_type : {
+        type : DataTypes.ENUM('video', 'image')
+    }
+});
