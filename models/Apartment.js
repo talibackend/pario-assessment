c6674c742049c7c36e8df41641fab21aa2bc5959
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2022-08-21 20:26:46 +0100

    Added Apartment Model.

---
+++ models/Apartment.js
@@ -0,0 +1,21 @@
+const {Model, DataTypes} = require("sequelize");
+const dbInstance = require("../database/sequelize");
+
+class Apartment extends Model {}
+
+Apartment.init({
+    id : {
+        type : DataTypes.INTEGER,
+        autoIncrement : true,
+        allowNull : false,
+        primaryKey : true
+    },
+    address : {
+        type : DataTypes.TEXT,
+        allowNull : false
+    },
+    landlord : {
+        type : DataTypes.STRING,
+        allowNull : true
+    }
+});
