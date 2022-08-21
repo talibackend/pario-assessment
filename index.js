a9788640e464772bd5487e06af532a200fab3151
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2022-08-21 21:37:12 +0100

    Now listening.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const UserModel = require("./models/User");
 const TokenModel = require("./models/Token");
 const ApartmentModel = require("./models/Apartment");
 const apartments = require("./database/apartments");
+const express = require("express");
+const app = express();
+const httpServer = require("http");
 
 
 dbInstance.sync({force : true}).then(async ()=>{
@@ -12,6 +15,6 @@ dbInstance.sync({force : true}).then(async ()=>{
         await ApartmentModel.create({address : apartments[i].address, landlord : apartments[i].landlord});
     }
     console.log("Data has been seeded into db.");
+    app.listen(process.env.PORT, ()=>{console.log(`Server running on :${process.env.PORT}`)});
+    httpServer.createServer(app);
 }).catch(err=>console.log(err))
-
-
