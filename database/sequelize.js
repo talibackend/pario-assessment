0543f76a4c684be52662bdcb3201d72f8a5e4302
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2022-08-22 02:43:51 +0100

    Update sequelize.js

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,4 +1,8 @@
 const Sequelize = require("sequelize");
-// module.exports = new Sequelize(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, {dialect : "postgres", database : process.env.DB_NAME, port : process.env.DB_PORT});
-module.exports = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {ssl : true});
+module.exports = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  }});
 
